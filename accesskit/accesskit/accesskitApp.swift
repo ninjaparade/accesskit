255b0c7fa271f9c09634dc0bcc8a3b58fad05f73
tylerwetrust <mucanbarli1@etf.unsa.ba>
2023-04-13 15:49:15 +0200

    Init project

---
+++ accesskit/accesskit/accesskitApp.swift
@@ -0,0 +1,17 @@
+//
+//  accesskitApp.swift
+//  accesskit
+//
+//  Created by user239070 on 4/13/23.
+//
+
+import SwiftUI
+
+@main
+struct accesskitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
